48b31733ded17b09996609e6cf4ea669d5632825
Eduardo <eduardo.cm@hotmail.com>
2020-02-14 21:48:28 -0300

    Adicionando projeto Spring Boot Udesc

---
+++ minicurso-spring-boot-udesc/src/main/java/com/example/myorder/services/RestaurantService.java
@@ -0,0 +1,50 @@
+package com.example.myorder.services;
+
+import com.example.myorder.api.dtos.CreateRestaurantDto;
+import com.example.myorder.api.dtos.RestaurantResponseDto;
+import com.example.myorder.entities.Restaurant;
+import com.example.myorder.exceptions.NotFoundException;
+import com.example.myorder.repositories.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RestaurantService {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    public void createRestaurant(CreateRestaurantDto createRestaurantDto) {
+        //TODO validações
+
+        Restaurant restaurant = new Restaurant()
+                .setEmail(createRestaurantDto.getEmail())
+                .setName(createRestaurantDto.getName())
+                .setPhone(createRestaurantDto.getPhone());
+
+        restaurantRepository.save(restaurant);
+    }
+
+    public RestaurantResponseDto getById(Integer id) {
+        Restaurant restaurant = findById(id);
+        return new RestaurantResponseDto()
+                .setId(restaurant.getId())
+                .setEmail(restaurant.getEmail())
+                .setName(restaurant.getName())
+                .setPhone(restaurant.getPhone());
+    }
+
+    public Restaurant findById(Integer id) {
+        Optional<Restaurant> optional = restaurantRepository.findById(id);
+
+        if(!optional.isPresent()) {
+            throw new NotFoundException("Restaurante não encontrado");
+        }
+
+        Restaurant restaurant = optional.get();
+        return restaurant;
+    }
+
+}
